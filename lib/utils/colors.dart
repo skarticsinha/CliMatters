61da086ed08d08c5f2f4d8f553f1d04fe0fc4e38
Aayush Ashok Kashyap <aak.ayush001@gmail.com>
2023-11-18 14:00:16 +0530

    Fixed Build problem

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColor = Color.fromRGBO(255, 255, 255, 1);
+const webBackgroundColor = Color.fromRGBO(255, 255, 255, 1);
+const mobileSearchColor = Color.fromRGBO(255, 255, 255, 1);
+const blueColor = Color.fromRGBO(78, 174, 238, 1);
+const primaryColor = Color.fromRGBO(19, 221, 127, 0.612);
+const secondaryColor = Colors.grey;
